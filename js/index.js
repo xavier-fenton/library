934d92c032bc6946a721288fbed7f9ee85def657
Xavier Fenton <xavierfenton99@gmail.com>
2022-10-19 11:54:38 +1300

    figuring js code: done for the day

--- js/index.js
+++ js/index.js
@@ -6,21 +6,35 @@ para.appendChild(node)
 const element = document.getElementById('div1')
 element.appendChild(para)
 
-
 //I want to display this data on the page without manually coding html
-
 
 const dummyData = {
   book: {
-    title: 'Macdancer',
+    title: 'MacDancer',
     author: 'MacDancerSoftwareEnterprise',
     pages: 'neverending',
     read: 'infinitley',
   },
 }
 
+//this displays the title in the DOM as a placeholder
+
+let display = new Array(dummyData.book)
+console.log(display)
+let keys = display.map((x) => `<p>${x.title}</p>` `<p>${x.author}</p>` )
+console.log(keys)
+document.getElementById('div1').innerHTML += 'Book: ' + display
+
+//Create a for loop to access each key in the obj of dummy data
+
+let nested = dummyData.book
+console.log(Object.keys(nested))
+for (let i = 0; i < nested.length; i++) {
+  nested[i]
+}
+
 let myLibrary = [dummyData.book]
-console.log('this is my library ', myLibrary)
+// console.log('this is my library ', myLibrary)
 
 const info = new Book('Lord of the Rings', 'Xavier Golem', 5000, '5 times')
 function Book(title, author, pages, read) {
