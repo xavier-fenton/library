235c51c936ddca3e6f3e00a522be2f7c5214e51e
Xavier Fenton <xavierfenton99@gmail.com>
2022-10-19 11:12:24 +1300

    add code to show appending to the DOM

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+//this code is an example of how I add to the DOM
+
+const para = document.createElement('p')
+const node = document.createTextNode('Where I want to display Library:')
+para.appendChild(node)
+const element = document.getElementById('div1')
+element.appendChild(para)
+
+
+//I want to display this data on the page without manually coding html
+
+
 const dummyData = {
   book: {
     title: 'Macdancer',
